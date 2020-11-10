9ead7c75df517875b3f904163e9333e967e7ed5b
Arkadiusz Codilime <arkadiusz.krolak@codilime.com>
2020-11-10 13:15:10 +0100

    Implemented Kentik Client API scaffolding.

--- kentik_fluent_api/kentik_api_client/kentik_api_client.py
+++ kentik_fluent_api/kentik_api_client/kentik_api_client.py
@@ -1,25 +1,18 @@
-from python_http_client import Client
-
-BASE_API_COM_URL = "https://api.kentik.com/api"
-BASE_API_EU_URL = "https://api.kentik.eu/api"
-
-GLOBAL_HEADERS_TEMPLATE = {
-    "X-CH-Auth-Email": None,
-    "X-CH-Auth-API-Token": None,
-    "Content-Type": "application/json"
-}
+from typing import Optional
+from queries.query import Query
 
 
-def get_kentik_client(base_url, auth_email, auth_api_token):
-    global_headers = GLOBAL_HEADERS_TEMPLATE
-    global_headers["X-CH-Auth-Email"] = auth_email
-    global_headers["X-CH-Auth-API-Token"] = auth_api_token
-    return Client(host=base_url, request_headers=global_headers)
+class KentikAPIClient:
+    def __init__(self, auth_email: str, auth_token: str):
+        pass
 
+    def send_call(self, query: Query, payload: Optional[type]):
+        pass
 
-def get_kentik_com_client(auth_email, auth_api_token):
-    return get_kentik_client(BASE_API_COM_URL, auth_email, auth_api_token)
+    @staticmethod
+    def get_synchronous_client():
+        pass
 
-
-def get_kentik_eu_client(auth_email, auth_api_token):
-    return get_kentik_client(BASE_API_EU_URL, auth_email, auth_api_token)
+    @staticmethod
+    def get_asynchronous_client(self):
+        pass
