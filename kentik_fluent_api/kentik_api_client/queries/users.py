597cc75b9d4493e6fb77f875496e5278ee8df45a
mradaszewski <maciej.radaszewski@gmail.com>
2020-11-10 11:21:50 +0100

    Added users queries implementation

---
+++ kentik_fluent_api/kentik_api_client/queries/users.py
@@ -0,0 +1,33 @@
+from query_decorators import get, post, put, delete, payload_type
+from query import Query
+
+
+@get
+def get_users() -> Query:
+    """Returns an array of users objects that each contain information about an individual user."""
+    return Query("/users")
+
+@get
+def get_user_info(user_id: int) -> Query:
+    """Returns a user object containing information about an individual user"""
+    url_path = f"/user/{user_id}"
+    return Query(url_path)
+
+@post
+@payload_type(dict)
+def create_user() -> Query:
+    """Creates and returns a user object containing information about an individual user"""
+    return Query("/user")
+
+@put
+@payload_type(dict)
+def update_user(user_id: int) -> Query:
+    """Updates and returns a user object containing information about an individual user"""
+    url_path = f"/user/{user_id}"
+    return Query(url_path)
+
+@delete
+def delete_user(user_id: int) -> Query:
+    """Deletes a user."""
+    url_path = f"/user/{user_id}"
+    return Query(url_path)
