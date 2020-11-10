48aba581d29232bf529ba62f9a2799bbdbb21e49
mradaszewski <maciej.radaszewski@gmail.com>
2020-11-10 15:41:01 +0100

    Added device_labels queries implementation

---
+++ kentik_api_library/src/queries/device_labels.py
@@ -0,0 +1,33 @@
+from query_decorators import get, post, put, delete, payload_type
+from query import Query
+
+
+@get
+def get_device_labels() -> Query:
+    """Returns an array of device_labels objects that each contain information about an individual device_label."""
+    return Query("/deviceLabels")
+
+@get
+def get_device_label_info(device_label_id: int) -> Query:
+    """Returns a device_label object containing information about an individual device_label"""
+    url_path = f"/deviceLabels/{device_label_id}"
+    return Query(url_path)
+
+@post
+@payload_type(dict)
+def create_device_label() -> Query:
+    """Creates and returns a device_label object containing information about an individual device_label"""
+    return Query("/deviceLabels")
+
+@put
+@payload_type(dict)
+def update_device_label(device_label_id: int) -> Query:
+    """Updates and returns a device_label object containing information about an individual device_label"""
+    url_path = f"/deviceLabels/{device_label_id}"
+    return Query(url_path)
+
+@delete
+def delete_device_label(device_label_id: int) -> Query:
+    """Deletes a device_label."""
+    url_path = f"/deviceLabels/{device_label_id}"
+    return Query(url_path)
