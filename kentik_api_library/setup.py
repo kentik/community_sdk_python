5c35bf2715283cca2149f1df25eb09472d5084ac
Dawid Trzebiatowski <boscode@boscard.pl>
2020-11-16 15:09:43 +0100

    First approach to package build using github actions

---
+++ kentik_api_library/setup.py
@@ -0,0 +1,20 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "Readme.md").read_text()
+
+setup(
+    name="kentik-api",
+    version="0.0.1",
+    description="API Library to help using Kenti API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kentik/community_sdk_python/tree/main/kentik_api_library",
+    licence="GPL-3.0",
+    packages=["src"],
+    include_package_data=True
+)
