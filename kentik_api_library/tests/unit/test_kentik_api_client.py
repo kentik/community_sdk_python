4154fa2cdbb716c2cf1f95b9221db591bd40d1cc
mradaszewski <maciej.radaszewski@gmail.com>
2020-11-16 14:35:31 +0100

    Test refactor for improved mobility

--- kentik_api_library/tests/unit/test_kentik_api_client.py
+++ kentik_api_library/tests/unit/test_kentik_api_client.py
@@ -1,4 +1,4 @@
-from kentik_api_client import kentik_api_client
+import kentik_fluent_api_client
 from python_http_client import Client
 
 
@@ -12,7 +12,7 @@ def test_get_kentik_client__return_api_client():
     # DUMMY_API_URL, DUMMY_EMAIL, DUMMY_API_TOKEN
 
     # WHEN
-    client = kentik_api_client.get_kentik_client(DUMMY_API_URL, DUMMY_EMAIL, DUMMY_API_TOKEN)
+    client = kentik_fluent_api_client.get_kentik_client(DUMMY_API_URL, DUMMY_EMAIL, DUMMY_API_TOKEN)
 
     # THEN
     assert isinstance(client, Client)
@@ -23,11 +23,11 @@ def test_get_kentik_com_client__return_com_api_client():
     # DUMMY_EMAIL, DUMMY_API_TOKEN
 
     # WHEN
-    client = kentik_api_client.get_kentik_com_client(DUMMY_EMAIL, DUMMY_API_TOKEN)
+    client = kentik_fluent_api_client.get_kentik_com_client(DUMMY_EMAIL, DUMMY_API_TOKEN)
 
     # THEN
     assert isinstance(client, Client)
-    assert client.host == kentik_api_client.BASE_API_COM_URL
+    assert client.host == kentik_fluent_api_client.BASE_API_COM_URL
 
 
 def test_get_kentik_com_client__return_eu_api_client():
@@ -35,8 +35,8 @@ def test_get_kentik_com_client__return_eu_api_client():
     # DUMMY_EMAIL, DUMMY_API_TOKEN
 
     # WHEN
-    client = kentik_api_client.get_kentik_eu_client(DUMMY_EMAIL, DUMMY_API_TOKEN)
+    client = kentik_fluent_api_client.get_kentik_eu_client(DUMMY_EMAIL, DUMMY_API_TOKEN)
 
     # THEN
     assert isinstance(client, Client)
-    assert client.host == kentik_api_client.BASE_API_EU_URL
+    assert client.host == kentik_fluent_api_client.BASE_API_EU_URL
