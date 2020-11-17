04710cb027a147ce6315f482bd822cd3d548f58b
Arkadiusz Codilime <arkadiusz.krolak@codilime.com>
2020-11-17 11:52:22 +0100

    Fixed kentik_apli_client.git

--- kentik_api_library/src/kentik_api_client.py
+++ kentik_api_library/src/kentik_api_client.py
@@ -6,8 +6,8 @@ import requests
 
 # Local application imports
 from auth import auth
-from queries.query import Query
-from queries.query import QueryType
+from api_calls.api_call import APICall
+from api_calls.api_call import APICallMethods
 
 
 class KentikAPIClient:
@@ -17,20 +17,19 @@ class KentikAPIClient:
         self._api_url = api_url
         self._auth = auth.KentikAuth(auth_email, auth_token)
 
-    def send_query(self, query: Query, payload: Optional[type] = None):
+    def send_query(self, query: APICall, payload: Optional[type] = None):
 
         _payload: Optional[dict] = payload if isinstance(payload, dict) else None
         url = self._get_api_query_url(query.url_path)
-        if query.method == QueryType.GET:
+        if query.method == APICallMethods.GET:
             return requests.get(url, auth=self._auth, headers=self.DEFAULT_HEADERS, params=_payload)
-        if query.method == QueryType.POST:
+        if query.method == APICallMethods.POST:
             return requests.get(url, auth=self._auth, headers=self.DEFAULT_HEADERS, data=payload)
-        if query.method == QueryType.PUT:
+        if query.method == APICallMethods.PUT:
             return requests.get(url, auth=self._auth, headers=self.DEFAULT_HEADERS, data=payload)
-        if query.method == QueryType.DELETE:
+        if query.method == APICallMethods.DELETE:
             return requests.get(url, auth=self._auth, headers=self.DEFAULT_HEADERS, data=payload)
 
     def _get_api_query_url(self, api_method: str):
         return self._api_url + api_method
-
 
