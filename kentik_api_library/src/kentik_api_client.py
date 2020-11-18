13b0050291c944243450d3bb9a83fcf7cdf69250
Arkadiusz Codilime <arkadiusz.krolak@codilime.com>
2020-11-18 12:40:10 +0100

    Added exception support for improper API call method. Improved names inkentik api klient.

--- kentik_api_library/src/kentik_api_client.py
+++ kentik_api_library/src/kentik_api_client.py
@@ -17,18 +17,19 @@ class KentikAPIClient:
         self._api_url = api_url
         self._auth = KentikAuth(auth_email, auth_token)
 
-    def send_query(self, query: APICall, payload: Optional[type] = None):
+    def send_query(self, api_call: APICall, payload: Optional[type] = None):
 
         _payload: Optional[dict] = payload if isinstance(payload, dict) else None
-        url = self._get_api_query_url(query.url_path)
-        if query.method == APICallMethods.GET:
+        url = self._get_api_query_url(api_call.url_path)
+        if api_call.method == APICallMethods.GET:
             return requests.get(url, auth=self._auth, headers=self.DEFAULT_HEADERS, params=_payload)
-        if query.method == APICallMethods.POST:
+        if api_call.method == APICallMethods.POST:
             return requests.get(url, auth=self._auth, headers=self.DEFAULT_HEADERS, data=payload)
-        if query.method == APICallMethods.PUT:
+        if api_call.method == APICallMethods.PUT:
             return requests.get(url, auth=self._auth, headers=self.DEFAULT_HEADERS, data=payload)
-        if query.method == APICallMethods.DELETE:
+        if api_call.method == APICallMethods.DELETE:
             return requests.get(url, auth=self._auth, headers=self.DEFAULT_HEADERS, data=payload)
+        raise ValueError("Improper API call method")
 
     def _get_api_query_url(self, api_method: str):
         return self._api_url + api_method
