b039c881f79c049cbe5cea0f2440176f136f41a4
arkadiusz.krolak <arkadiusz.krolak@codilime.com>
2020-11-27 00:27:59 +0100

    KNTK-77 Device request payload data structure

---
+++ kentik_api_library/kentik_api/requests_payload/notset.py
@@ -0,0 +1,20 @@
+"""This module provides default types and values for uninitialized fields of request's payload classes
+and also mypy tool's type error suppression for special cases."""
+from typing import Any
+
+
+class NOTSET:
+    """Default type for unset fields."""
+
+    def __repr__(self):
+        return "NOTSET_"
+
+
+_NOTSET: NOTSET = NOTSET()
+"""Default value for unset fields."""
+
+_NOTSET_ = Any
+"""mypy tool's type error suppression and indicator of expected real value."""
+
+_None_ = Any
+"""mypy tool's type error suppression and indicator of expected real value."""
