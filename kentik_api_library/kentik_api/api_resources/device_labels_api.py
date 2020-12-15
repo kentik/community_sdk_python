907cdcd4d6b52f1a0b9508cc0882ad4a8c835689
Mateusz Midor <3demaniac@gmail.com>
2020-12-15 08:42:13 +0100

    Removed redundant labels implementation

--- kentik_api_library/kentik_api/api_resources/device_labels_api.py
+++ kentik_api_library/kentik_api/api_resources/device_labels_api.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from kentik_api.api_calls import labels
+from kentik_api.api_calls import device_labels
 from kentik_api.api_connection.api_connector_protocol import APIConnectorProtocol
 from kentik_api.public.device_label import DeviceLabel
 from kentik_api.requests_payload import labels_payload, as_dict
@@ -18,29 +18,29 @@ class DeviceLabelsAPI:
         return self._api_connector.send(api_call, payload)
 
     def get_all(self) -> List[DeviceLabel]:
-        apicall = labels.get_labels()
+        apicall = device_labels.get_device_labels()
         response = self._send(apicall)
         return labels_payload.GetAllResponse.from_json(response.text).to_device_labels()
 
     def get(self, label_id: int) -> DeviceLabel:
-        apicall = labels.get_label_info(label_id)
+        apicall = device_labels.get_device_label_info(label_id)
         response = self._send(apicall)
         return labels_payload.GetResponse.from_json(response.text).to_device_label()
 
     def create(self, device_label: DeviceLabel) -> DeviceLabel:
         assert device_label.color is not None
-        apicall = labels.create_label()
+        apicall = device_labels.create_device_label()
         payload = labels_payload.CreateRequest(device_label.name, device_label.color)
         response = self._send(apicall, payload)
         return labels_payload.CreateResponse.from_json(response.text).to_device_label()
 
     def update(self, device_label: DeviceLabel) -> DeviceLabel:
-        apicall = labels.update_label(device_label.id)
+        apicall = device_labels.update_device_label(device_label.id)
         payload = labels_payload.UpdateRequest(device_label.name, device_label.color)
         response = self._send(apicall, payload)
         return labels_payload.UpdateResponse.from_json(response.text).to_device_label()
 
     def delete(self, label_id: int) -> bool:
-        apicall = labels.delete_label(label_id)
+        apicall = device_labels.delete_device_label(label_id)
         response = self._send(apicall)
         return labels_payload.DeleteResponse.from_json(response.text).success
