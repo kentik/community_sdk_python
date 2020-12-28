c5c1856c60572b178baad79d10370f1994cf7807
lukaa12 <lukasz.wolanin@codilime.com>
2020-12-28 09:36:31 +0100

    Added dataclass decorators in public/plan.py

--- kentik_api_library/kentik_api/requests_payload/plans_payload.py
+++ kentik_api_library/kentik_api/requests_payload/plans_payload.py
@@ -18,5 +18,9 @@ class GetAllResponse:
         plans = self.plans
         for plan in plans:
             plan["deviceTypes"] = [PlanDeviceType(**i) for i in plan["deviceTypes"]]
-            plan["devices"] = [PlanDevice(**i) for i in plan["devices"]]
+            plan["devices"] = [PlanDevice(
+                device_name=i["device_name"],
+                device_type=i["device_type"],
+                id=int(i["id"]),
+            ) for i in plan["devices"]]
         return [Plan(**i) for i in plans]
