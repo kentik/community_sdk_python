cca4e4a5c57f219b55d6ea3353c68279eb9044ca
Mateusz Midor <3demaniac@gmail.com>
2021-01-15 13:39:32 +0100

    Made PlansAPI inherit from BaseAPI

--- kentik_api_library/kentik_api/api_resources/plans_api.py
+++ kentik_api_library/kentik_api/api_resources/plans_api.py
@@ -1,19 +1,16 @@
 from http import HTTPStatus
 from typing import List
 
+from kentik_api.api_resources.base_api import BaseAPI
 from kentik_api.api_calls import plan
-from kentik_api.api_connection.api_connector_protocol import APIConnectorProtocol
 from kentik_api.public.plan import Plan
 from kentik_api.requests_payload import plans_payload
 
 
-class PlansAPI:
+class PlansAPI(BaseAPI):
     """Exposes Kentik API operations related to plans. """
-
-    def __init__(self, api_connector: APIConnectorProtocol) -> None:
-        self._api_connector = api_connector
 
     def get_all(self) -> List[Plan]:
         api_call = plan.get_plans()
-        response = self._api_connector.send(api_call)
+        response = self.send(api_call)
         return plans_payload.GetAllResponse.from_json(response.text).to_plans()
