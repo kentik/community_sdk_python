3d825dbfcbec9244cd10b8085e306c15d32e6306
Mateusz Midor <3demaniac@gmail.com>
2021-01-22 14:57:56 +0100

    Implemented active waiting retry + demo in jupyter notebook

---
+++ kentik_api_library/kentik_api/commands/cmd.py
@@ -0,0 +1,12 @@
+from typing import Any, Callable
+
+
+class Cmd:
+    """ Command desing pattern - wrap method call into an object for easy queuing and retrying """
+
+    def __init__(self, method: Callable, **method_params) -> None:
+        self._method = method
+        self._params = method_params
+
+    def execute(self) -> Any:
+        return self._method(**self._params)
