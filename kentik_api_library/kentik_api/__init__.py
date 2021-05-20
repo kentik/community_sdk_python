e3d208d0bb1abc8359fb64c35c716fc62b415b5c
martin <martin@kentik.com>
2021-05-19 22:21:36 -0700

    Added class RetryableSession (based on requests.Session) Refactored APIConnector to use RetryableSession Added support for specifying connection, request read-timeout and retry strategy while constructing KentikAPI

--- kentik_api_library/kentik_api/__init__.py
+++ kentik_api_library/kentik_api/__init__.py
@@ -1,2 +1,3 @@
 from .kentik_api import KentikAPI
 from .public import *
+from requests.packages.urllib3.util.retry import Retry
--- kentik_api_library/kentik_api/kentik_api.py
+++ kentik_api_library/kentik_api/kentik_api.py
@@ -12,7 +12,8 @@ from .api_resources.sites_api import SitesAPI
 from .api_resources.tags_api import TagsAPI
 from .api_resources.tenants_api import MyKentikPortalAPI
 from .api_resources.users_api import UsersAPI
-
+from typing import Optional, Tuple, Union
+from .api_connection.retryable_session import Retry
 
 # pylint: disable=too-many-instance-attributes
 
@@ -23,8 +24,10 @@ class KentikAPI:
     API_URL_EU = "https://api.kentik.eu/api/v5"
     API_URL_US = "https://api.kentik.com/api/v5"
 
-    def __init__(self, auth_email: str, auth_token: str, api_url: str = API_URL_US) -> None:
-        connector = new_connector(api_url, auth_email, auth_token)
+    def __init__(self, auth_email: str, auth_token: str, api_url: str = API_URL_US,
+                 timeout: Union[float, Tuple[float, float]] = (10.0, 60.0),
+                 retry_strategy: Optional[Retry] = None) -> None:
+        connector = new_connector(api_url, auth_email, auth_token, timeout, retry_strategy)
 
         self.device_labels = DeviceLabelsAPI(connector)
         self.sites = SitesAPI(connector)
@@ -44,5 +47,6 @@ class KentikAPI:
 # pylint: enable=too-many-instance-attributes
 
 
-def new_connector(api_url: str, auth_email: str, auth_token: str) -> APIConnector:
-    return APIConnector(api_url, auth_email, auth_token)
+def new_connector(api_url: str, auth_email: str, auth_token: str, timeout: Union[float, Tuple[float, float]],
+                  retry_strategy: Optional[Retry]) -> APIConnector:
+    return APIConnector(api_url, auth_email, auth_token, timeout, retry_strategy)
