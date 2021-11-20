535256f9af8750f92304880cb49cfd67138805b8
martin <martin@kentik.com>
2021-11-19 22:12:14 -0800

    Add support for storing proxy and URL in profile file Improve logging of failures to parse profile file.

--- kentik_api_library/kentik_api/utils/auth.py
+++ kentik_api_library/kentik_api/utils/auth.py
@@ -1,37 +1,55 @@
 import json
 import logging
 import os
+from functools import cache
 from typing import Dict, Optional, Tuple
 
+log = logging.getLogger("auth")
 
-def load_credential_profile(filename: str) -> Optional[Dict]:
+
+@cache
+def load_credential_profile(profile: str) -> Dict:
+    home: str = os.environ.get("KTAPI_HOME", os.environ.get("HOME", "."))
+    filename = os.environ.get("KTAPI_CFG_FILE", os.path.join(home, ".kentik", profile))
     try:
         with open(filename) as f:
-            cfg = json.load(f)
+            try:
+                cfg = json.load(f)
+            except json.decoder.JSONDecodeError as ex:
+                log.critical("Failed to parse JSON in profile file '%s' (exception: %s)", filename, ex)
+                return {}
     except OSError as e:
-        logging.critical("Cannot open config file: %s (%s)", filename, str(e))
-        return None
+        log.critical("Cannot open config file: %s (%s)", filename, str(e))
+        return {}
     ok: bool = True
+    # check for required keys
     for key in ("email", "api-key"):
         if key not in cfg:
-            logging.critical("No '%s' in config file: %s", key, filename)
+            log.critical("No '%s' in config file: %s", key, filename)
             ok = False
     if ok:
         return cfg
     else:
-        return None
+        return {}
 
 
 def get_credentials(profile: str = "default") -> Tuple[str, str]:
     email = os.environ.get("KTAPI_AUTH_EMAIL")
     token = os.environ.get("KTAPI_AUTH_TOKEN")
-    home: str = os.environ.get("KTAPI_HOME", os.environ.get("HOME", "."))
     if email is None or token is None:
-        cfg = load_credential_profile(os.environ.get("KTAPI_CFG_FILE", os.path.join(home, ".kentik", profile)))
-        if cfg is not None:
+        cfg = load_credential_profile(profile)
+        if cfg:
             email = cfg["email"]
             token = cfg["api-key"]
     if email and token:
         return email, token
     else:
         raise RuntimeError("Failed to get API authentication credentials")
+
+
+def get_url(profile: str = "default") -> Optional[str]:
+    return os.environ.get("KTAPI_URL", load_credential_profile(profile).get("url"))
+
+
+def get_proxy(profile: str = "default") -> Optional[str]:
+    return os.environ.get("KTAPI_PROXY", load_credential_profile(profile).get("proxy"))
